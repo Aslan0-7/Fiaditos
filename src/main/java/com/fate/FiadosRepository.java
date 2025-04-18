49432d29e5cb1789924d53cd66af1fbd041a9a45
Franchesca Yuridia <franchesca.murrugarra.s@gmail.com>
2025-04-17 21:36:59 -0500

    añadiendo los metodos

--- src/main/java/com/fate/FiadosRepository.java
+++ src/main/java/com/fate/FiadosRepository.java
@@ -1,5 +1,11 @@
 package com.fate;
 
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /***
  * This class is the responsible for the interaction with the database
  * It should:
@@ -13,8 +19,29 @@ package com.fate;
  * 8. filter by item
  */
 public class FiadosRepository {
-    public void addClient() {
-        System.out.println("Hola,estoy probando nada mas :c");
+    private Connection conn;
+
+    public FiadosRepository(Connection conn) {
+        this.conn = conn;
     }
 
+    public void addClient(Integer id , String cliente) throws SQLException {
+        String sql = "INSERT INTO fiados(id, cliente) VALUES(?, ?)";
+        try (PreparedStatement  pstmt = conn.prepareStatement(sql)) {
+            pstmt.setInt(1, id);
+            pstmt.setString(2, cliente);
+            pstmt.executeUpdate();
+            System.out.println("Se añadió el cliente " + cliente + " a la tiendita :D");
+        }
+    }
+
+    public void deleteClient(String cliente) throws SQLException {
+        String sql = "DELETE FROM fiados WHERE cliente = ?";
+        try (PreparedStatement  pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, cliente);
+            pstmt.executeUpdate();
+
+            System.out.println("Se elimino a: "+ cliente+ " de la tiendita porque no pagaba a tiempo :c");
+        }
+    }
 }
