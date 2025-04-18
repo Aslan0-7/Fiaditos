937ee2267cc968606c5cb2fd70726294d8820ab6
Franchesca Yuridia <franchesca.murrugarra.s@gmail.com>
2025-04-17 22:29:29 -0500

    metodo para actualizar fiados de un cliente

--- src/main/java/com/fate/Main.java
+++ src/main/java/com/fate/Main.java
@@ -25,7 +25,9 @@ public class Main {
             //metodos.addClient(111, "Franchesca");
             //metodos.deleteClient("Haziz");
 
-            metodos.añadirCliente();
+            //metodos.añadirCliente();
+
+            metodos.updateFiados("Francheca", "bebidas", "CocaCola 1L",4,4.50);
 
 
 
@@ -47,31 +49,10 @@ public class Main {
 
                         System.out.println("El estado del cliente  " + cliente + " es : " + estado);
                     }
-                }
-
-                /*
-
-                    try (Statement stmt3 = conn.createStatement()) {
-                    stmt3.executeUpdate("INSERT INTO fiados(id, cliente) VALUES(55,'Kisame')");
-                    stmt3.executeUpdate("INSERT INTO fiados(id, cliente) VALUES(56,'Mitsuki')");
-
-                    System.out.println("Se añadieron nuevos clientes a la tiendita :D");
-
                 }*/
-            }
 
 
-
-           /* try (Statement stmt3 = conn.createStatement()) {
-                stmt3.executeUpdate("UPDATE fiados SET producto = 'atun' WHERE cliente = 'Aslan'");
-                stmt3.executeUpdate("UPDATE fiados SET precioventa = 2.50 WHERE producto = 'atun'");
-                stmt3.executeUpdate("UPDATE fiados SET cantidad = 4 WHERE producto = 'atun'");
-                stmt3.executeUpdate("DELETE FROM fiados WHERE id=112");
-
-                System.out.println("Un cliente fue eliminado de la tiendita :c");
-
-            }
-
+/*
             try (Statement stmt4 = conn.createStatement()) {
                 stmt4.executeUpdate("UPDATE fiados SET total = precioventa * cantidad");
                 ResultSet rs3 = stmt4.executeQuery("SELECT cliente,producto,total FROM fiados WHERE cliente = 'Aslan' ");
@@ -84,7 +65,7 @@ public class Main {
             }
         }*/
 
-
+   }
 }
 
 // New comentario
