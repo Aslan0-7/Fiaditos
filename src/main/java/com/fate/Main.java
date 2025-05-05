d55faab002de2eea44b83504c31ed01ce61ebab4
Erikson Murrugarra <erikson.murrugarra@gmail.com>
2025-05-06 01:08:15 +0200

    TAREA #1

--- src/main/java/com/fate/Main.java
+++ src/main/java/com/fate/Main.java
@@ -1,15 +1,31 @@
 package com.fate;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        FiadoEntity fiado = new FiadoEntity(1, "Federico", 10);
+//        FiadoEntity fiado = new FiadoEntity(2, "Franchesca", 10);
+//        DbConnector connector = new DbConnector();
+//        FiadoRepository fiadoRepository = new FiadoRepository(connector);
 
-        DbConnector connector = new DbConnector();
 
-        FiadoRepository fiadoRepository = new FiadoRepository(connector);
+//        fiadoRepository.insert(fiado);
 
-        fiadoRepository.insert(fiado);
+        System.out.println("===== Sistema de Fiaditos ====== ");
+        System.out.println("[1] Agregar Cliente");
+        System.out.println("[2] Eliminar Cliente");
+        System.out.println("[4] Listar Clientes");
+        System.out.println("[5] Cerrar Sistema");
+        System.out.println("Ingrese una opcion: ");
+
+        Scanner scanner = new Scanner(System.in);
+        int opcion = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("La opcion es " + opcion);
+
+        // while (opcion != 5)
+        // opcion < 1 || opcion > 5: no exixste la opcion, ingresa de nuevo.
 
     }
 }
