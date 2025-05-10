1cdc02b93d3040e146e86dd0cc22b76aa9c7fe2f
Franchesca Yuridia <franchesca.murrugarra.s@gmail.com>
2025-05-09 20:07:02 -0500

    3.-Agregando lo que le falta a la entidad fiados

--- src/main/java/com/fate/FiadoEntity.java
+++ src/main/java/com/fate/FiadoEntity.java
@@ -3,18 +3,29 @@ package com.fate;
 public class FiadoEntity {
     private Integer id;
     private String cliente;
-    private Integer cantidad;
+    private String categoria;
+    private String producto;
+    private String unidad;
+    private Double cantidad;
+    private Double precioventa;
+    private Double total;
+    private String estado;
 
-    public FiadoEntity(Integer id, String cliente, Integer cantidad) {
+    public FiadoEntity(Integer id, String cliente, String categoria, String producto, String unidad, Double cantidad, Double precioventa, Double total, String estado ) {
         this.id = id;
         this.cliente = cliente;
+        this.categoria = categoria;
+        this.producto = producto;
+        this.unidad = unidad;
         this.cantidad = cantidad;
+        this.precioventa = precioventa;
+        this.total = total;
+        this.estado = estado;
     }
 
     public Integer getId() {
         return id;
     }
-
     public void setId(Integer id) {
         this.id = id;
     }
@@ -22,16 +33,19 @@ public class FiadoEntity {
     public String getCliente() {
         return cliente;
     }
-
     public void setCliente(String cliente) {
         this.cliente = cliente;
     }
 
-    public Integer getCantidad() {
-        return cantidad;
-    }
+    public String getCategoria(){return categoria;}
+    public void setCategoria(String categoria){this.categoria = categoria;}
 
-    public void setCantidad(Integer cantidad) {
-        this.cantidad = cantidad;
-    }
+    public String getProducto(){return producto;}
+    public void setProducto(String producto){this.producto = producto;}
+
+    public String getUnidad(){return unidad;}
+    public void setUnidad(String unidad){this.unidad = unidad;}
+
+    public Double getCantidad() {return cantidad;}
+    public void setCantidad(Double cantidad) {this.cantidad = cantidad;}
 }
